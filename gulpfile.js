01882554fdab2a71740ea12d514bb4a9664ba15d
Rich Churcher <rich.churcher@gmail.com>
2015-07-05 11:02:13 +1200

    Restructure for minification/deploy

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,21 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var stripDebug = require('gulp-strip-debug');
 var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
+//var sourcemaps = require('gulp-sourcemaps');
  
 gulp.task('js', function () {
   return gulp.src([
       '!js/all.js',
-      'js/**/*.js' 
+      'js/lib/jquery.min.js',
+      'js/lib/bootstrap.min.js',
+      'js/lib/d3.min.js',
+      'js/src/*.js'
     ])
-    .pipe(sourcemaps.init())
-      .pipe(concat('js/all.js'))
-      //.pipe(stripDebug())
-      .pipe(uglify(true))
-    .pipe(sourcemaps.write())
+    //.pipe(sourcemaps.init())
+    .pipe(concat('js/all.js'))
+    //.pipe(stripDebug())
+    .pipe(uglify(true))
+    //.pipe(sourcemaps.write())
     .pipe(gulp.dest('.'));
 });
 
