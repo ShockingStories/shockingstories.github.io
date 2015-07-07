c8b723b31f6f42a0c9b0ccb464f55035b7715bea
Rich Churcher <rich.churcher@gmail.com>
2015-07-07 20:56:05 +1200

    Remove js for publish

--- js/src/ui.js
+++
@@ -1,9 +0,0 @@
-// NB: translated to avoid using JQuery, since we don't need it for anything else
-function powerOff() {
-  document.body.style.display = 'none';
-  document.getElementsByTagName('html')[0].style.backgroundColor = 'black';
-  setTimeout(function(){
-    document.body.style.display = 'block';
-    document.getElementsByTagName('html')[0].style.backgroundColor = 'none';
-  }, 1000);
-}
